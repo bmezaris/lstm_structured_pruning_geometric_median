4ada9703e4cffdf3444c7961aa0070f2ef24f287
gkaln <55789275+gkaln@users.noreply.github.com>
2021-01-04 22:51:14 +0200

    Add files via upload

---
+++ model/mymodels.py
@@ -0,0 +1,42 @@
+""""
+Implementation in Tensorflow of the BLSTM model described in:
+N. Gkalelis and V. Mezaris, “Fractional step discriminant pruning:
+A filter pruning framework for deep convolutional neural networks,”
+in IEEE ICMEW, London, UK, Jul. 2020, pp. 1–6.
+History
+-------
+DATE      | DESCRIPTION    | NAME              | Organization |
+1/07/2020 | first creation | Nikolaos Gkalelis | CERTH-ITI    |
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+class MyLstm(tf.keras.Model):
+    def __init__(self, num_classes,
+                 moe_l2=0.005,
+                 weight_decay = 0.005,
+                 sequence_length = 300,
+                 lstm_size = 1024,
+                 video_feature_size = 1024,
+                 audio_feature_size = 128):
+        super(MyLstm, self).__init__()
+
+        total_feature_size = video_feature_size + audio_feature_size
+        input_shape = (sequence_length, total_feature_size)
+        lstm_fw = tf.keras.layers.LSTM(int(lstm_size / 2), return_sequences=True)
+        lstm_bw = tf.keras.layers.LSTM(int(lstm_size / 2), return_sequences=True, go_backwards=True)
+        self.bdr = tf.keras.layers.Bidirectional(lstm_fw, backward_layer=lstm_bw, input_shape=input_shape)
+        self.lstm = tf.keras.layers.LSTM(int(lstm_size))
+        self.dns = tf.keras.layers.Dense(units=num_classes, activation='sigmoid',
+                                  kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+
+
+    def call(self, inputs):
+
+        x1 = self.bdr(inputs)
+        x2 = self.lstm(x1)
+        x = self.dns(x2)
+
+        return x, x1, x2
